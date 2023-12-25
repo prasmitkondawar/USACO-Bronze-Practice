dac7d7a6787934d5afe1c83179cda6525f7775ae
Prasmit Kondawar <kondawarprasmit@gmail.com>
2023-12-25 14:38:38 -0800

    solved The Lost Cow

--- Simulation/TheLostCow.cpp
+++ Simulation/TheLostCow.cpp
@@ -6,17 +6,28 @@ using namespace std;
 
 int main() 
 {
+    // For getting input from input.txt file
+   freopen("lostcow.in", "r", stdin);
+    // Printing the Output to output.txt file
+   freopen("lostcow.out", "w", stdout);
     int x, y;
     cin >> x >> y;
 
     int totDis = 0;
     int dis = 1;
     int sign = 1;
-    while(x != y) {
+    int xi = x;
+    while((!(y >= x && y <= xi) && !(y <= x && y >= xi)) && (xi != y)) {
         int xf = x + (dis * sign);
-        dis++;
+        dis *= 2;
         sign *= -1;
-        totDis += abs(xf - x);
-        x = xf;
+        totDis += abs(xf - xi);
+        xi = xf;
     }
+    
+    if(xi != y) {
+        totDis -= abs(xi - y);
+    }
+
+    cout << totDis << "\n";
 }
