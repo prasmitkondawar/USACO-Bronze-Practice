8388631a8190a38f003ac54cbd05418f3cacf1f3
Prasmit Kondawar <kondawarprasmit@gmail.com>
2024-01-02 18:59:31 -0800

    8/10 test cases passed for Tic Tac Toe

--- Simulation/Submitted/TicTacToe.cpp
+++ Simulation/Submitted/TicTacToe.cpp
@@ -1,3 +1,4 @@
+//passed 8/10 cases (faild)
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -15,15 +16,15 @@ int main()
 
     vector<vector<char>> board(3);
     vector<char> letters;
-    vector<bool> hasUsed(26, true);
+    vector<bool> hasUsed(26, false);
     for(int i = 0; i < 3; i++) {
         string s;
         cin >> s;
+        board[i].resize(3);
         for(int j = 0; j < 3; j++) {
             board[i][j] = s[j];
-            board.resize(3);
-            if(!hasUsed[int(s[j]) - 97]) {
-                hasUsed[int(s[j]) - 97] = false;
+            if(!hasUsed[int(s[j]) - 65]) {
+                hasUsed[int(s[j]) - 65] = true;
                 letters.push_back(s[j]);
             }
         }
@@ -31,27 +32,28 @@ int main()
 
     //finding single digit answer
     int ans = 0;
-    // for(int i = 0; i < letters.size(); i++) {
-    //     char c = letters[i];
-    //     vector<vector<bool>> present(3);
-    //     for(int j = 0; j < 3; j++) {
-    //         present.resize(3);
-    //         for(int k = 0; k < 3; k++) {
-    //             if(c == board[j][k]) {
-    //                 present[j][k] = true;
-    //             }
-    //         }
-    //     }
+    for(int i = 0; i < letters.size(); i++) {
+        char c = letters[i];
+        vector<vector<bool>> present(3);
+        for(int j = 0; j < 3; j++) {
+            present[j].resize(3);
+            for(int k = 0; k < 3; k++) {
+                present[j][k] = false;
+                if(c == board[j][k]) {
+                    present[j][k] = true;
+                }
+            }
+        }
 
-    //     if(present[0][0] && present[0][1] && present[0][2]) ans++;
-    //     else if(present[1][0] && present[1][1] && present[1][2]) ans++;
-    //     else if(present[2][0] && present[2][1] && present[2][2]) ans++;
-    //     else if(present[2][1] && present[1][1] && present[2][1]) ans++;
-    //     else if(present[0][2] && present[1][2] && present[2][2]) ans++;
-    //     else if(present[0][0] && present[1][0] && present[2][0]) ans++;
-    //     else if(present[0][0] && present[1][1] && present[2][2]) ans++;
-    //     else if(present[0][2] && present[1][1] && present[2][0]) ans++;
-    // }
+        if(present[0][0] && present[0][1] && present[0][2]) ans++;
+        else if(present[1][0] && present[1][1] && present[1][2]) ans++;
+        else if(present[2][0] && present[2][1] && present[2][2]) ans++;
+        else if(present[0][1] && present[1][1] && present[2][1]) ans++;
+        else if(present[0][2] && present[1][2] && present[2][2]) ans++;
+        else if(present[0][0] && present[1][0] && present[2][0]) ans++;
+        else if(present[0][0] && present[1][1] && present[2][2]) ans++;
+        else if(present[0][2] && present[1][1] && present[2][0]) ans++;
+    }
 
     // vector<bool> present(letters.size(), false);
     // if(board[0][0] == board[0][1]  && board[0][1] == board[0][2] && ) ans++;
@@ -67,20 +69,86 @@ int main()
 
     //finding double team answer
     ans = 0;
-    for(int counter = 0; counter < letters.size(); counter++) {
+    for(int counter = 0; counter < letters.size() - 1; counter++) {
         char c = letters[counter];
-        for(int i = counter; i < letters.size(); i++) {
+        for(int i = counter + 1; i < letters.size(); i++) {
             char c2 = letters[i];
-            vector<vector<bool>> board(3);
+            bool x = false;
+            bool y = false;
+            bool found = false;
+            for(int rows = 0; rows < 3; rows++) {
+                for(int col = 0; col < 3; col++) {
+                    if(board[rows][col] == c) {
+                        x = true;
+                    } else if(board[rows][col] == c2) {
+                        y = true;
+                    } else {
+                        x = false;
+                        y = false;
+                        break;
+                    }
+                }
 
-            if((board[0][0] == c || board[0][0] == c2) && (board[0][1] == c || board[0][1] == c2) && (board[0][2] == c || board[0][2] == c2)) ans++;
-            else if((board[1][0] == c || board[1][0] == c2) && (board[1][1] == c || board[1][1] == c2) && (board[1][2] == c || board[1][2] == c2)) ans++;
-            else if((board[2][0] == c || board[2][0] == c2) && (board[2][1] == c || board[2][1] == c2) && (board[2][2] == c || board[2][2] == c2)) ans++;
-            else if((board[0][0] == c || board[0][0] == c2) && (board[1][0] == c || board[1][0] == c2) && (board[2][0] == c || board[2][0] == c2)) ans++;
-            else if((board[0][1] == c || board[0][1] == c2) && (board[1][1] == c || board[1][1] == c2) && (board[2][1] == c || board[2][1] == c2)) ans++;
-            else if((board[0][2] == c || board[0][2] == c2) && (board[1][2] == c || board[1][2] == c2) && (board[2][2] == c || board[2][2] == c2)) ans++;
-            else if((board[0][0] == c || board[0][0] == c2) && (board[1][1] == c || board[1][1] == c2) && (board[2][2] == c || board[2][2] == c2)) ans++;
-            else if((board[2][0] == c || board[2][0] == c2) && (board[1][1] == c || board[1][1] == c2) && (board[0][2] == c || board[0][2] == c2)) ans++;
+                if(x && y) {
+                    found = true;
+                    break;
+                }
+            }
+
+            for(int rows = 0; rows < 3; rows++) {
+                for(int col = 0; col < 3; col++) {
+                    if(board[col][rows] == c) {
+                        x = true;
+                    } else if(board[col][rows] == c2) {
+                        y = true;
+                    } else {
+                        x = false;
+                        y = false;
+                        break;
+                    }
+                }
+
+                if(x && y) {
+                    found = true;
+                    break;
+                }
+            }
+
+            for(int dia = 0; dia < 3; dia++) {
+                if(board[dia][dia] == c) {
+                    x = true;
+                } else if (board[dia][dia] == c2) {
+                    y = true;
+                } else {
+                    x = false;
+                    y = false;
+                    break;
+                }
+
+            }
+
+            if(x && y) {
+                    found = true;
+            }
+
+            for(int dia = 0; dia < 3; dia++) {
+                if(board[2 - dia][dia] == c) {
+                    x = true;
+                } else if (board[2 - dia][dia] == c2) {
+                    y = true;
+                } else {
+                    x = false;
+                    y = false;
+                    break;
+                }
+            
+            }
+
+            if(x && y) {
+                found = true;
+            }
+
+            if(found) ans++;
         }
     }
 
